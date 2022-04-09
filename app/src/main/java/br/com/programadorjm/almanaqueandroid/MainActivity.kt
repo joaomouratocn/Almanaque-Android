2327138dad6938a175d3755387217f1545912c5e
João Mourato <joaomouratocn@gmail.com>
2022-04-09 06:52:37 -0300

    Configurado navegação pela toolbar

--- app/src/main/java/br/com/programadorjm/almanaqueandroid/MainActivity.kt
+++ app/src/main/java/br/com/programadorjm/almanaqueandroid/MainActivity.kt
@@ -2,10 +2,35 @@ package br.com.programadorjm.almanaqueandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import br.com.programadorjm.almanaqueandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.mainToolbar)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
+        if (navHost != null){
+            navController = navHost.findNavController()
+        }
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
